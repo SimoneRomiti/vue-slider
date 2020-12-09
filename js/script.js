20b8ca09b8a030ccbdf66504a29f0084ed6a223b
Simone Romiti <sr1992ei@gmail.com>
2020-12-09 15:56:22 +0100

    NextImage fatto

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var app = new Vue(
+  {
+    el: "#slider",
+    data: {
+      imageIndex: 0,
+      imageUrl: [
+        "img/redbull.jpg",
+        "img/mercedes2.jpg",
+        "img/ferrari2.jpg",
+        "img/racingpoint.jpg",
+        "img/mclaren.jpg",
+       ],
+    },
+    methods: {
+      nextImage: function(){
+        if(this.imageIndex + 1 == this.imageUrl.length){
+          this.imageIndex = 0;
+        } else {
+          this.imageIndex++;
+        }
+
+      }
+    }
+  }
+);
