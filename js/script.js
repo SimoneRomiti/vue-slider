6fdd19b542ad722b339141110aa98f7a46c5af2c
Simone Romiti <sr1992ei@gmail.com>
2020-12-09 17:18:10 +0100

    Esercizio con Bonus

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var app = new Vue(
   {
     el: "#slider",
+
     data: {
       imageIndex: 0,
       imageUrl: [
@@ -10,23 +11,43 @@ var app = new Vue(
         "img/racingpoint.jpg",
         "img/mclaren.jpg",
        ],
+       active: true,
     },
+
+    created: function(){
+      auto = setInterval(
+        () => {
+          this.nextImage();
+        }, 3000
+      );
+    },
+
     methods: {
+      clickNextImage : function(){
+        clearInterval(auto);
+        this.nextImage();
+      },
+
+      clickPrevImage: function(){
+        clearInterval(auto);
+        this.prevImage();
+      },
+
       nextImage: function(){
         if(this.imageIndex + 1 == this.imageUrl.length){
           this.imageIndex = 0;
         } else {
           this.imageIndex++;
         }
-
       },
+      
       prevImage: function(){
         if(this.imageIndex - 1 == -1){
           this.imageIndex = this.imageUrl.length - 1;
         } else {
           this.imageIndex--;
         }
-      }
+      },
     }
   }
 );
